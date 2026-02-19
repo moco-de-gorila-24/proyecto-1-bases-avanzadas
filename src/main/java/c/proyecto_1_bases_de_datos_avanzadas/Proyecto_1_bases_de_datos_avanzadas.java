4c4165dd7caac6fac1ca431f42f272e66084565a
Luis Alonso <luisalonsooritzaguirre8@gmail.com>
2026-02-19 00:56:22 -0700

    primer commit del proyecto

---
+++ src/main/java/c/proyecto_1_bases_de_datos_avanzadas/Proyecto_1_bases_de_datos_avanzadas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package c.proyecto_1_bases_de_datos_avanzadas;
+
+/**
+ *
+ * @author Central
+ */
+public class Proyecto_1_bases_de_datos_avanzadas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
