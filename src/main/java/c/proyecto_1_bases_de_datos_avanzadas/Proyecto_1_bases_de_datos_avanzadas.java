4b6dc1c04c4d88ebb07d266fa61422d56088d3d0
Luis Alonso <luisalonsooritzaguirre8@gmail.com>
2026-02-22 11:48:32 -0700

    la verdad llevo rato si usar git y se ma paso hacerle git add a todo

--- src/main/java/c/proyecto_1_bases_de_datos_avanzadas/Proyecto_1_bases_de_datos_avanzadas.java
+++ src/main/java/c/proyecto_1_bases_de_datos_avanzadas/Proyecto_1_bases_de_datos_avanzadas.java
@@ -4,13 +4,44 @@
 
 package c.proyecto_1_bases_de_datos_avanzadas;
 
+import dominio.Cliente;
+import negocio.PersistenciaException;
+import paneles.Login;
+import paneles.Register;
+import persistenciaBD.ConexionBD;
+import persistenciaBD.IConexionBD;
+import persistenciaDAO.ClienteDAO;
+import persistenciaDAO.IClienteDAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  *
- * @author Central
+ * @author Luis
  */
 public class Proyecto_1_bases_de_datos_avanzadas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Register login = new Register();
+        //login.mostrar();
+
+        Login logi = new Login();
+        logi.mostrar();
+
+//        try{
+//            IConexionBD conexion = new ConexionBD();
+//            IClienteDAO clienteDAO = new ClienteDAO(conexion);
+//
+           Cliente cliente = new Cliente(3, "Charly","Duarte", "Mendez" , Date.valueOf("2005-03-21"),"kino", "obregonyork",85175);
+//
+//            clienteDAO.agregarCliente(cliente);
+//        }
+//        catch (PersistenciaException e) {
+//            throw new RuntimeException(e);
+//        }
+
     }
 }
