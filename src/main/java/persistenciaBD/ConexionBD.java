6b03b8fa4feec0e54043ea6354ff95c38062b767
Luis Alonso <luisalonsooritzaguirre8@gmail.com>
2026-02-22 11:52:16 -0700

    Agregue la logica para la persistencia de la conexion de la base de datos

--- src/main/java/persistenciaBD/ConexionBD.java
+++ src/main/java/persistenciaBD/ConexionBD.java
@@ -1,4 +1,18 @@
 package persistenciaBD;
 
-public class ConexionBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD implements IConexionBD{
+
+    private final String CADENA_CONEXION = "jdbc:mysql://localhost:3306/pizzeria";
+    private final String USUARIO = "root";
+    private final String CONTRASENIA = "3082006Luis";
+
+
+    @Override
+    public Connection crearConexion() throws SQLException {
+        return DriverManager.getConnection(CADENA_CONEXION, USUARIO, CONTRASENIA);
+    }
 }
--- src/main/java/persistenciaBD/IConexionBD.java
+++ src/main/java/persistenciaBD/IConexionBD.java
@@ -1,4 +1,4 @@
-package persistencia;
+package persistenciaBD;
 
 import java.sql.Connection;
 import java.sql.SQLException;
