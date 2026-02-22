cba216245e9a4dc96a9dac62458c0841f5240084
Luis Alonso <luisalonsooritzaguirre8@gmail.com>
2026-02-22 11:55:40 -0700

    Clase de que hereda de exception para atrapar excepciones de la parte de negocio

--- src/main/java/negocio/NegocioException.java
+++ src/main/java/negocio/NegocioException.java
@@ -1,4 +1,15 @@
 package negocio;
 
-public class NegocioException {
+public class NegocioException extends Exception {
+    public NegocioException(){
+
+    }
+
+    public NegocioException(String message){
+        super(message);
+    }
+
+    public NegocioException(String message, Throwable cause){
+        super(message, cause);
+    }
 }
