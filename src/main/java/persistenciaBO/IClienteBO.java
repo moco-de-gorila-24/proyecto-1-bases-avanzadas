e68f93ebb456b2c655882d182e4a73eb43cfb93b
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-02-24 01:40:34 -0700

    Cambie el metodo agregar en su parametro

--- src/main/java/persistenciaBO/IClienteBO.java
+++ src/main/java/persistenciaBO/IClienteBO.java
@@ -1,4 +1,10 @@
 package persistenciaBO;
 
+import DTO.ClienteDTO;
+import dominio.Cliente;
+import negocio.NegocioException;
+import negocio.PersistenciaException;
+
 public interface IClienteBO {
+    public Cliente agregarCliente(ClienteDTO cliente) throws NegocioException, PersistenciaException;
 }
